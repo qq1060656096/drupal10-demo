c48ced41743f6b0f53b24b555e2cb4f1384940e7
zhao <1060656096@qq.com>
2023-06-24 09:36:26 +0800

    ccc: demo

--- modules/hello2023/src/Controller/AdminController.php
+++ modules/hello2023/src/Controller/AdminController.php
@@ -3,6 +3,8 @@
 namespace Drupal\hello2023\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
+use Drupal\Core\Link;
 
 class AdminController extends ControllerBase
 {
@@ -13,12 +15,65 @@ class AdminController extends ControllerBase
       'Desc',
       'Link',
     ];
-    $rows = [];
-    $rows[] = [
-      'Hello2023',
-      'Hello2023 Desc',
-      \Drupal::l('Hello2023 Link', Url::fromRoute('hello2023.admin')),
+    $list = [
+      [
+        'name' => 'page hello',
+        'desc' => 'page hello Desc',
+        'route_name' => 'hello2023.page.hello',
+      ],
+      [
+        'name' => 'page html',
+        'desc' => 'page html Desc',
+        'route_name' => 'hello2023.page.html',
+      ],
+      [
+        'name' => 'page form',
+        'desc' => 'page form Desc',
+        'route_name' => 'hello2023.page.form',
+      ],
+      [
+        'name' => 'page table',
+        'desc' => 'page table Desc',
+        'route_name' => 'hello2023.page.table',
+      ],
+      [
+        'name' => 'page dbTable',
+        'desc' => 'page dbTable Desc',
+        'route_name' => 'hello2023.page.dbTable',
+      ],
+      [
+        'name' => 'json hello',
+        'desc' => 'json hello Desc',
+        'route_name' => 'hello2023.json.hello',
+      ],
+      [
+        'name' => 'json dbTable',
+        'desc' => 'json dbTable Desc',
+        'route_name' => 'hello2023.json.dbTable',
+      ],
+      [
+        'name' => 'router hello',
+        'desc' => 'router hello Desc',
+        'route_name' => 'hello2023.router.hello',
+        'router_param' => ['name'=>'张三'],
+      ],
+      [
+        'name' => 'router hello2',
+        'desc' => 'router hello2 Desc',
+        'route_name' => 'hello2023.router.hello2',
+      ],
     ];
+    $rows = [];
+    foreach ($list as $row) {
+      $parameter = isset($row['router_param']) ? $row['router_param'] : [];
+      $url = Url::fromRoute($row['route_name'], $parameter);
+      $link = Link::fromTextAndUrl('view', $url)->toString();
+      $rows[] = [
+        $row['name'],
+        $row['desc'],
+        $link,
+      ];
+    }
 
     $build = [];
     // Build the table
